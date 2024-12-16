5e733e9dad9473a1ec1fb5fb1f71927dd4f21c4c
Tácio Wurdig <wurdigtm@outlook.com>
2024-12-16 01:11:21 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      'preto': '#393D3F',
+      'cinza': '#B9B6B5',
+      'ciano': '#62929E',
+      'grullo': '#AA8E7E'
+    },
+    fontFamily: {
+      'poppins': 'Poppins, sans-serif',
+      'raleway': 'Raleway, sans-serif' 
+    },
+    extend: {},
+  },
+  plugins: [],
+}
